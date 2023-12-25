382b0712d84f246d39733fc9832afb76fec65461
Anna <annak0366@gmail.com>
2023-12-25 23:47:11 +0100

    adding config files

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,40 +1,40 @@
-exports.config = {
-    user: process.env.BROWSERSTACK_USERNAME || 'anna_rFw9Wf',
-    key: process.env.BROWSERSTACK_ACCESS_KEY || 'R7svfN1pu97ZCSXfaTEw',
+// exports.config = {
+//     user: process.env.BROWSERSTACK_USERNAME || 'anna_rFw9Wf',
+//     key: process.env.BROWSERSTACK_ACCESS_KEY || 'R7svfN1pu97ZCSXfaTEw',
   
-    updateJob: false,
-    specs: [
-      './test/specs/*.js',
-    ],
-    exclude: [],
+//     updateJob: false,
+//     specs: [
+//       './test/specs/*.js',
+//     ],
+//     exclude: [],
   
-    capabilities: [{
-      project: "First Webdriverio Android Project",
-      build: 'Webdriverio Android',
-      name: 'first_test',
-      device: 'Google Pixel 3',
-      os_version: "9.0",
-      'browserstack.app_url': 'bs://1a8d8e5a52223246a97f1f580e64b4a3ca3541da',
-      app: 'bs://1a8d8e5a52223246a97f1f580e64b4a3ca3541da',
-      'browserstack.debug': true,
-      showDeviceScreen: true
+//     capabilities: [{
+//       project: "First Webdriverio Android Project",
+//       build: 'Webdriverio Android',
+//       name: 'first_test',
+//       device: 'Google Pixel 3',
+//       os_version: "9.0",
+//       'browserstack.app_url': 'bs://1a8d8e5a52223246a97f1f580e64b4a3ca3541da',
+//       app: 'bs://1a8d8e5a52223246a97f1f580e64b4a3ca3541da',
+//       'browserstack.debug': true,
+//       showDeviceScreen: true
       
-    }],
+//     }],
 
-    require: ['ts-node/register'],
+//     require: ['ts-node/register'],
     
   
-    logLevel: 'info',
-    coloredLogs: true,
-    screenshotPath: './errorShots/',
-    baseUrl: '',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
+//     logLevel: 'info',
+//     coloredLogs: true,
+//     screenshotPath: './errorShots/',
+//     baseUrl: '',
+//     waitforTimeout: 10000,
+//     connectionRetryTimeout: 90000,
+//     connectionRetryCount: 3,
   
-    framework: 'mocha',
-    mochaOpts: {
-      ui: 'bdd',
-      timeout: 20000
-    }
-  };
+//     framework: 'mocha',
+//     mochaOpts: {
+//       ui: 'bdd',
+//       timeout: 20000
+//     }
+//   };
